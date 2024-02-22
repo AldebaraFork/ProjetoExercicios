3a72f5c93ffd64f481f5d57d7f318cd757836ea6
AldebaraFork <eduardoamorim758@proton.me>
2024-02-21 21:59:40 -0300

    Exercicio de estrutura FOR  (Recomendo uma pesquisa para melhor entendimento)

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Exercicio da estrutura de condicional FOR
+
+using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            for (int i = int.Parse(Console.ReadLine()); i <= 100; i++)
+            {
+                
+                Console.WriteLine("O valor de i é: " +i);
+            }
+        }
+    }
+}
+
