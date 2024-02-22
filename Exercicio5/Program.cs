4aad679aacc537974a5cf6fda2e5b2abdc9faac3
AldebaraFork <eduardoamorim758@proton.me>
2024-02-22 15:32:50 -0300

    Trabalhando com vetores e estrutura for

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            string[] nomes = new string [10];
+            int[] idade = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Digite o nome da pessoa ");
+                nomes[i] = Console.ReadLine();
+            }
+
+
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Digite a idade da pessoa ");
+                idade[i] = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
