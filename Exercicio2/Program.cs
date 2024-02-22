40e460e05afcf29b5e52a927bd4d0e5d40d76b8c
AldebaraFork <eduardoamorim758@proton.me>
2024-02-21 21:25:30 -0300

    Exercicio de estrutura While

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Codigo basico de um programa em C#
+
+//Estrutura de repetição wh
+using System;
+using System.Runtime.Serialization;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, soma;
+
+            soma = 0;
+
+            while (soma <= 10)
+            {
+               soma = soma + 1;
+                Console.WriteLine("Digite o numero: ");
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("A soma dos numeros digitados é: " + soma);
+        }
+    }
+}
